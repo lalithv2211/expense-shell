9d14f0939282154fe891a3c9906ccbbca10677e8
sailalith <lalithvoop@hotmail.com>
2024-06-04 00:25:47 +0530

    added a new function to handle error status in common.sh and edited the backend.sh

--- backend.sh
+++ backend.sh
@@ -9,64 +9,64 @@ fi
 
 printing_the_header "Disable default NodeJS Version Module"
 dnf module disable nodejs -y &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "Enable NodeJS module for V20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "Install NodeJS"
 dnf install nodejs -y &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "Remove old files"
 rm -rf /app &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "copying the service file"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "adding user"
 useradd expense &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "creating a new /app folder"
 mkdir /app &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "downloading the backend zip file"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "unzipping the downloaded zip file"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "npm install packages"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "daemon-reload step"
 systemctl daemon-reload &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "starting the backend service"
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "installing the mysql"
 dnf install mysql -y &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "connecting to Database"
 mysql -h 172.31.27.94 -uroot -p$db_installation_password < /app/schema/backend.sql &>>/tmp/expense.log
-echo $?
+print_error_status $?
 
 printing_the_header "restarting the backend service"
 systemctl restart backend &>>/tmp/expense.log
 systemctl status backend &>>/tmp/expense.log
-echo $?
+print_error_status $?
--- common.sh
+++ common.sh
@@ -1,3 +1,11 @@
 printing_the_header() {
   echo $1
 }
+
+print_error_status() {
+  if [ $1 -eq 0 ]; then
+    echo "SUCCESS"
+  else
+    echo "FAILURE"
+  fi
+}
