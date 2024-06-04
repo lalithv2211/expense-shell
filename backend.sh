d172881dab58971f922723a187c40358c1342127
sailalith <lalithvoop@hotmail.com>
2024-06-04 21:32:39 +0530

    reverting backend again to test

--- backend.sh
+++ backend.sh
@@ -2,9 +2,6 @@ source common.sh
 
 db_installation_password=$1
 
-app_dir=/app
-component=backend
-
 if [ -z "$db_installation_password" ]; then
   echo "password is empty, please rerun with correct password param"
   exit 1
@@ -29,7 +26,16 @@ else
 fi
 
 printing_the_header "Install NodeJS"
-dnf install nodejs -y &>>$log_file 
+dnf install nodejs -y &>>$log_file
+if [ $? -eq 0 ]; then
+  print_error_status $?
+else
+  print_error_status $?
+  exit 2
+fi
+
+printing_the_header "Remove old files"
+rm -rf /app &>>$log_file
 if [ $? -eq 0 ]; then
   print_error_status $?
 else
@@ -52,7 +58,43 @@ if [ $? -ne 0 ]; then
   useradd expense &>>$log_file
 fi
 
-app_reqs
+printing_the_header "creating a new /app folder"
+mkdir /app &>>$log_file
+if [ $? -eq 0 ]; then
+  print_error_status $?
+else
+  print_error_status $?
+  exit 2
+fi
+
+printing_the_header "downloading the backend zip file"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$log_file
+if [ $? -eq 0 ]; then
+  print_error_status $?
+else
+  print_error_status $?
+  exit 2
+fi
+
+printing_the_header "unzipping the downloaded zip file"
+cd /app &>>$log_file
+unzip /tmp/backend.zip &>>$log_file
+if [ $? -eq 0 ]; then
+  print_error_status $?
+else
+  print_error_status $?
+  exit 2
+fi
+
+printing_the_header "npm install packages"
+cd /app &>>$log_file
+npm install &>>$log_file
+if [ $? -eq 0 ]; then
+  print_error_status $?
+else
+  print_error_status $?
+  exit 2
+fi
 
 printing_the_header "daemon-reload step"
 systemctl daemon-reload &>>$log_file
@@ -83,7 +125,7 @@ else
 fi
 
 printing_the_header "connecting to Database"
-mysql -h 172.31.21.22 -uroot -p$db_installation_password < /app/schema/backend.sql &>>$log_file
+mysql -h 172.31.24.127 -uroot -p$db_installation_password < /app/schema/backend.sql &>>$log_file
 if [ $? -eq 0 ]; then
   print_error_status $?
 else
