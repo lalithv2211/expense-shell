2a15641a6bd31854fb8ebcfac4cd7a9225f98053
sailalith <lalithvoop@hotmail.com>
2024-06-04 21:34:33 +0530

    added app_reqs and missing step "npm install packages" to backend.sh

--- backend.sh
+++ backend.sh
@@ -2,6 +2,9 @@ source common.sh
 
 db_installation_password=$1
 
+app_dir=/app
+component=backend
+
 if [ -z "$db_installation_password" ]; then
   echo "password is empty, please rerun with correct password param"
   exit 1
@@ -34,15 +37,6 @@ else
   exit 2
 fi
 
-printing_the_header "Remove old files"
-rm -rf /app &>>$log_file
-if [ $? -eq 0 ]; then
-  print_error_status $?
-else
-  print_error_status $?
-  exit 2
-fi
-
 printing_the_header "copying the service file"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
@@ -58,33 +52,7 @@ if [ $? -ne 0 ]; then
   useradd expense &>>$log_file
 fi
 
-printing_the_header "creating a new /app folder"
-mkdir /app &>>$log_file
-if [ $? -eq 0 ]; then
-  print_error_status $?
-else
-  print_error_status $?
-  exit 2
-fi
-
-printing_the_header "downloading the backend zip file"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  print_error_status $?
-else
-  print_error_status $?
-  exit 2
-fi
-
-printing_the_header "unzipping the downloaded zip file"
-cd /app &>>$log_file
-unzip /tmp/backend.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  print_error_status $?
-else
-  print_error_status $?
-  exit 2
-fi
+app_reqs
 
 printing_the_header "npm install packages"
 cd /app &>>$log_file
@@ -125,7 +93,7 @@ else
 fi
 
 printing_the_header "connecting to Database"
-mysql -h 172.31.24.127 -uroot -p$db_installation_password < /app/schema/backend.sql &>>$log_file
+mysql -h 172.31.21.22 -uroot -p$db_installation_password < /app/schema/backend.sql &>>$log_file
 if [ $? -eq 0 ]; then
   print_error_status $?
 else
